ea4d06c050291d27c02ea14ce2d68f9c1676594c
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-12-01 22:13:00 +0530

    Format Specifiers in Python

---
+++ format_in_python.py
@@ -0,0 +1,5 @@
+# How to print format data in python 
+
+ 
+print("This is %s -->OLD METHOD" % cool.name)                     #2.7
+print("This is {} -> NEW METHOD".format(cool.name))               #3.6+
