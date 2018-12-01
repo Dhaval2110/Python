b5f54faddaa89eb557df7627b744489837723815
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-12-02 00:26:46 +0530

    getattr

--- getattr.py
+++ getattr.py
@@ -7,3 +7,15 @@ class Person:
 person = Person()
 print('The age is:', getattr(person, "age"))
 print('The age is:', person.age)
+
+
+# how to define switch case
+
+# def switch(case):
+# 	switcher={
+# 	'Name':'Dhaval',
+# 	'Class':'A1',
+# 	'No':10}
+# 	print(switcher.get(case))
+
+# switch('Name')
