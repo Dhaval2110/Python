ea534186176f8aaab355aa1d0376d0449cac8bbc
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-12-02 22:37:39 +0530

    Factoriab+Fibonacci

--- factorial.py
+++ factorial.py
@@ -14,4 +14,20 @@ print(s)
 a, b = 0, 1
 while b < 100:
     print(b)
-    a, b = b, a + b
+    a, b = b, a + b 
+	
+	
+	
+	#OR
+
+
+def fib(n):
+	if n==0 or n==1:
+		return 1
+	else:
+		return fib(n-1)+fib(n-2)
+
+n=10
+for i in range(0,n):
+	s=fib(i)
+	print(s)
