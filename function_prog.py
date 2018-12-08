b92c3291629428bacb72afd1ec071c17c4a986a7
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-12-08 13:41:59 +0530

    Functional Programming in Python
    Lambda , Map and Filter

--- function_prog.py
+++ function_prog.py
@@ -1,5 +1,6 @@
 # What is map ,filter and reduce aka function programming?
 
+#Using Traditional Function :)
 def abc(i):
 	if i%2==0 and i%10==0:
 		return i
@@ -9,3 +10,10 @@ print(list(map(abc,l)))
 print(list(filter(abc,l)))
 
 ## print((reduce(abc,l))) ## not sure
+
+#Using Lambda Function :)
+
+l=[i for i in range(0,500)]
+x=lambda x:x%2==0 and x%7==0
+print(list(map(x,l)))
+print(list(filter(x,l)))
