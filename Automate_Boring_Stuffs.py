25ef33856a9587de2f3535e0a9488c3330713bd0
Dhaval <mehtadhaval401@gmail.com>
2019-01-12 23:31:22 +0530

    Automate Boring Stuffs

--- Automate_Boring_Stuffs.py
+++ Automate_Boring_Stuffs.py
@@ -1,4 +1,14 @@
+# Automating webbrowser applications
+
+# Global Imports
 import webbrowser
 
+#Global variables
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-webbrowser.get(chrome_path).open("https://www.youtube.com/watch?v=U5GPvBErVVc")
+
+# 1. Automation Youtube
+# webbrowser.get(chrome_path).open("https://www.youtube.com/watch?v=U5GPvBErVVc") #Open URL in Chrome
+
+# 2. Facebook automation
+webbrowser.get(chrome_path).open(
+    "https://www.facebook.com/")  # If already logged in :(
