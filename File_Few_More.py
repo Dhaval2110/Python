8fa68a7a906b6e5fd00d163bc0427a5989d66c4c
Dhaval <mehtadhaval401@gmail.com>
2019-01-15 22:39:22 +0530

    Few more files

--- File_Few_More.py
+++ File_Few_More.py
@@ -1,13 +1,54 @@
+# File examples to read for 
+
+# Global Import
 import sys
 import os
 import time
 
-c=0
-with open(r'C:\Users\DHAVAL\Desktop\abc.txt','r') as f:
-	s=f.read()
-	print(s)
-	for i in s:
-		if i.isupper():
-			c+=1
+#Local Import 
 
-print(c,"count")
+
+# Global variables
+c=0
+
+# How to count no of uppercase in file
+
+# with open(r'C:\Users\DHAVAL\Desktop\abc.txt','r') as f:
+# 	s=f.read()
+# 	print(s)
+# 	for i in s:
+# 		if i.isupper():
+# 			c+=1
+
+# print(c,"count")
+
+#################################################################################
+
+# How to replace word in file
+
+with open(r'C:\Users\DHAVAL\Desktop\abc.txt','r') as fd:
+	#content=f.readlines()
+	#print(content)
+	l=fd.readlines()
+	#print(l)
+	with open(r'C:\Users\DHAVAL\Desktop\abc.txt','w') as f:
+		for i in l:
+			#print(i)
+			if 'webbrowser' in i:
+				#print('true')
+				m=i.replace('webbrowser','WOAAA')
+				f.write("%s"% m)
+			else:
+				f.write('%s'%i)
+	# for line in content:
+	# 	#print(line)
+	# 	#print(type(line))
+	# 	if "webbroser" in line:
+	# 		print("True")
+	# 		line=line.replace("webbroser","LOL")
+
+# with open(r'C:\Users\DHAVAL\Desktop\abc.txt','w') as f1:
+# 	f1.write('%s' %line)
+
+
+
