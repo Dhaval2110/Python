3e3c5bcd79e883309600bdb985b4d6fa4a6eab65
Dhaval <mehtadhaval401@gmail.com>
2019-01-15 22:58:08 +0530

    List operations

--- list_sort.py
+++ list_sort.py
@@ -1,12 +1,24 @@
-l=['dhaval','mehta','united','FCBarca','spain']
-s=[]
-count=65
-while len(s)<len(l):
-	for i in range(0,len(l)):
-		if ord(l[i][:1])==count:
-			s.append(l[i])
-			count+=1
-	count+=1
+# Different sorting methods
 
-print(s)
-print(sorted(l))
+#1. Sort a list by first alphabet
+
+# l=['dhaval','mehta','united','FCBarca','spain']
+# s=[]
+# count=65
+# while len(s)<len(l):
+# 	for i in range(0,len(l)):
+# 		if ord(l[i][:1])==count:
+# 			s.append(l[i])
+# 			count+=1
+# 	count+=1
+
+# print(s)
+# print(sorted(l))
+
+# How to reverse a string 
+s=list("Dhaval")
+s.reverse()
+print(''.join(s))
+##or
+s=s[::-1]
+print(''.join(s))
