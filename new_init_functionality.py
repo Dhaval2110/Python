2f94d849a69115a3eab6c6939a278f5d3420f8b7
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2019-03-12 22:10:31 +0530

    New and init functionality in OOPS

--- new_init_functionality.py
+++ new_init_functionality.py
@@ -1,6 +1,7 @@
 '''
 Reference : https://spyhce.com/blog/understanding-new-and-init
 '''
+'''
 class A(object):  # -> don't forget the object specified as base
 
     def __new__(cls):
@@ -11,3 +12,23 @@ class A(object):  # -> don't forget the object specified as base
         print "A.__init__ called"
 
 A()
+'''
+#best example in which first __new__ is called before __init__
+
+class ABC:
+	def __new__(cls,*args,**kwargs):
+		print("In a __new__")
+		print(cls,"Class")
+		print(*args,"Args")
+		#print(**kwargs,"kwargs")
+		return super(ABC,cls).__new__(cls)
+	def __init__(self,a,b):
+		print("In a __init__")
+		self.a=a
+		self.b=b
+	def printdata(self):
+	 	print(self.a,"a")
+	 	print(self.b,"b")
+
+c=ABC(10,20)
+c.printdata()
