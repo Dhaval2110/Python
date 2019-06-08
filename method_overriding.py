70b3bfddd37cfd7f761b1999718e99025df13d05
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2019-06-09 00:00:27 +0530

    Method Overriding(OOPs)

---
+++ method_overriding.py
@@ -0,0 +1,14 @@
+'''
+ Suppose class B inherits from class A. Both have the method sayhello()- to each, their own version. B overrides the sayhello() of class A. So, when we create an object of class B, it calls the version that class B has.
+'''
+
+class A:
+	def sayhello(self):
+		print("Hello, I'm A")
+class B(A):
+	def sayhello(self):
+		print("Hello, I'm B")
+a=A()
+b=B()
+a.sayhello()
+b.sayhello()
